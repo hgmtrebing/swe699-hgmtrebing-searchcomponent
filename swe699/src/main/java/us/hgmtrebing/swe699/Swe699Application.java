1d1b7d451f5afc1ce00d0ec2c16c94321083a3bf
hgmtrebing <hgmtrebing@outlook.com>
2020-02-13 19:46:47 -0500

    Initial Commit. Contains only generated code from Spring Initializr.

---
+++ swe699/src/main/java/us/hgmtrebing/swe699/Swe699Application.java
@@ -0,0 +1,13 @@
+package us.hgmtrebing.swe699;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe699Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe699Application.class, args);
+	}
+
+}
---
+++ swe699/src/test/java/us/hgmtrebing/swe699/Swe699ApplicationTests.java
@@ -0,0 +1,13 @@
+package us.hgmtrebing.swe699;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe699ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
