1140797e0a56741614f004696e0b8cf7a4579751
hgmtrebing <hgmtrebing@outlook.com>
2020-02-20 21:40:51 -0500

    Addition of primitive search page, located at the Web root. Configured Spring Boot to listen on Port 80, by default.

---
+++ src/main/java/us/hgmtrebing/swe699/Swe699Controller.java
@@ -0,0 +1,16 @@
+package us.hgmtrebing.swe699;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Swe699Controller {
+
+    /*
+    @GetMapping("/search")
+    public String search(Model model) {
+        return "search";
+    }
+     */
+}
--- src/main/java/us/hgmtrebing/swe699/Swe699RestController.java
+++ src/main/java/us/hgmtrebing/swe699/Swe699RestController.java
@@ -1,18 +1,25 @@
 package us.hgmtrebing.swe699;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Swe699RestController {
 
+    // hgt - removing so that index.html can be the entry point
+    /*
     @RequestMapping("/")
     public String index() {
         return "Hello World Buddies!";
     }
 
+     */
+
     @RequestMapping("/test")
     public String test() {
         return "Goodbye peeps!";
     }
+
 }
